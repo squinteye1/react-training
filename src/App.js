d3eecf8ca0ab3b387cdef813015e7dcdf9af608b
uk14767829 <neil.mckay@tesco.com>
2022-12-13 14:16:35 +0000

    passing properties down

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 //import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import DataEntryField from './components/DataEntryField';
 
 function App() {
+  const [vatValue, setVatValue] = useState('');
   return (
     <>
       <div className="App">
         <Header />
-        <DataEntryField title={"Price excl VAT:"} />
+        <DataEntryField
+          title={"Price excl VAT:"}
+          value={vatValue}
+          setValue={setVatValue}
+        />
+        <DataEntryField
+          title={"Price inc VAT:"}
+          value={vatValue}
+          setValue={setVatValue}
+        />
       </div>
     </>
   );
--- src/components/DataEntryField.jsx
+++ src/components/DataEntryField.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
 import TempDisplayField from './TempDisplayField';
 
 const DataEntryField = (props) => {
 
-    const [value, setValue] = useState(0);
+    //const [value, setValue] = useState(0);
   return (
     <div>
-        {props.title}
-        <input type="number" id="lname" name="lname" value={props.value} onChange={(e) => setValue(+e.target.value)} />
-        <TempDisplayField valueToDisplay={value} />
+      {props.title}
+      <input
+        type="number"
+        id="lname"
+        name="lname"
+        value={props.value}
+        onChange={(e) => props.setValue(+e.target.value)}
+      />
+      <TempDisplayField valueToDisplay={props.value} />
     </div>
-  )
+  );
 }
 
 export default DataEntryField
