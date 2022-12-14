c5403a01ff7e856adf5d05ff7397170471ff8259
uk14767829 <neil.mckay@tesco.com>
2022-12-14 14:24:09 +0000

    adding in routing

--- src/components/Sale.jsx
+++ src/components/Sale.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SaleItem from "./SaleItem.model";
+import { Link } from "react-router-dom";
 
 const Sale = ({ saleItem }) => {
   return (
     <tr>
-      <td>{saleItem.salePrice}</td>
-      <td>{saleItem.totalPrice}</td>
-      <td>{saleItem.vatToPay}</td>
-      <td>{saleItem.vatRate}</td>
+      <td>
+        <Link to="/">{saleItem.salePrice}</Link>
+      </td>
+      <td>
+        <Link to="/">{saleItem.totalPrice}</Link>
+      </td>
+      <td>
+        <Link to="/">{saleItem.vatToPay}</Link>
+      </td>
+      <td>
+        <Link to="/">{saleItem.vatRate}</Link>
+      </td>
     </tr>
   );
 };
