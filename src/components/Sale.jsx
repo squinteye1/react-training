7574deb051b26cabf984bfd027c9a90533c1e406
uk14767829 <neil.mckay@tesco.com>
2022-12-14 14:51:38 +0000

    added toast

--- src/components/Sale.jsx
+++ src/components/Sale.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import SaleItem from "./SaleItem.model";
 import { Link } from "react-router-dom";
+import Toast from "react-bootstrap/Toast";
 
 const Sale = ({ saleItem }) => {
+  const [showToast, setShowToast] = useState(false);
+
+  const toggleShowToast = () => setShowToast(!showToast);
+
+  const onClickHandler = (event) => {
+    let tagName = event.target.tagName;
+    console.log(
+      tagName === "TD" ? "Table Data" : tagName === "A" ? "Hyperlink" : "N/A"
+    );
+    if (tagName === "TD") toggleShowToast();
+  };
+
   return (
-    <tr>
-      <td>
-        <Link to="/">{saleItem.salePrice}</Link>
-      </td>
-      <td>
-        <Link to="/">{saleItem.totalPrice}</Link>
-      </td>
-      <td>
-        <Link to="/">{saleItem.vatToPay}</Link>
-      </td>
-      <td>
-        <Link to="/">{saleItem.vatRate}</Link>
-      </td>
-    </tr>
+    <>
+      <tr key={saleItem.id} onClick={onClickHandler}>
+       
+        <td>
+          <Link to="/">{saleItem.salePrice}</Link>
+        </td>
+        <td>
+          <Link to="/">{saleItem.totalPrice}</Link>
+        </td>
+        <td>
+          <Link to="/">{saleItem.vatToPay}</Link>
+        </td>
+        <td>{saleItem.vatRate}</td>
+      </tr>
+      <Toast show={showToast} onClose={toggleShowToast}>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Sale Item</strong>
+          <small>Details</small>
+        </Toast.Header>
+        <Toast.Body>
+          {`${saleItem.salePrice}` +
+            " => " +
+            `${saleItem.totalPrice}` +
+            " => " +
+            `${saleItem.vatToPay}`}
+        </Toast.Body>
+      </Toast>
+    </>
   );
 };
 
